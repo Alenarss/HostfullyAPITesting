6e6652963ef1cf245704c07819f49da1235a7976
Ali Enes Arslan <88050660+Alenarss@users.noreply.github.com>
2025-03-20 05:39:20 +0300

    Updated BaseUrlHostfully.java

--- src/test/java/BaseTest/BaseUrlHostfully.java
+++ src/test/java/BaseTest/BaseUrlHostfully.java
@@ -12,7 +12,7 @@ public class BaseUrlHostfully {
     @Before
     public void setup() {
         spec = new RequestSpecBuilder()
-                .setBaseUri("https://qa-assessment.svc.hostfully.com/bookings")
+                .setBaseUri("https://qa-assessment.svc.hostfully.com")
                 .setAuth(RestAssured.basic("candidate@hostfully.com", "NaX5k1wFadtkFf"))
                 .build();
     }
